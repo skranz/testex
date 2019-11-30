15647a9ed31700d2335df80fc9905290e0ca75fe
skranz <sebastian.kranz@uni-ulm.de>
2019-11-30 21:58:08 +0100

    updates

--- R/exceptions.r
+++ R/exceptions.r
@@ -1,10 +1,12 @@
 #' Create exemptions, i.e. functions or classes that will not be compared
 #'
-testex_exemptions = function(funs=NULL, classes=NULL, exempt.error.funs=NULL, global.exempt.funs = default.global.exempt.funs(), global.exempt.error.funs=NULL) {
-  list(funs = union(funs, global.exempt.funs), classes=classes, exempt.error.funs = union(exempt.error.funs, global.exempt.error.funs))
+testex_exemptions = function(funs=NULL, classes=NULL, exempt.error.funs=NULL, global.exempt.funs = default.global.exempt.funs(), global.exempt.error.funs=NULL, global.classes=default.global.exempt.classes()) {
+  list(funs = union(funs, global.exempt.funs), classes=union(classes, global.classes), exempt.error.funs = union(exempt.error.funs, global.exempt.error.funs))
 }
 
-
+default.global.exempt.classes = function() {
+  c("gg","ggplot")
+}
 
 default.global.exempt.funs = function() {
   c("library", "runif","rnorm","rexp")
