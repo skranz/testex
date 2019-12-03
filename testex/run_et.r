a3d618a6ad39ae64b1699bfda4ad6df429b83c0b
skranz <sebastian.kranz@uni-ulm.de>
2019-12-03 22:50:38 +0100

    test

--- testex/run_et.r
+++ testex/run_et.r
@@ -8,7 +8,16 @@ cat("\n", digest(ser,serialize=FALSE, skip=14))
 cat("\n",digest(serialize("Hello",NULL, version=2),serialize = FALSE, skip=14))
 cat("\nNow with refhook")
 cat("\n",digest(serialize("Hello",NULL,refhook = function(...) ""),serialize = FALSE, skip=14))
-cat("\Now ascii with and without refhook")
+
+
+cat("\nNow with and without refhook skipping 40")
+ser = serialize("Hello",NULL,refhook = function(...) "")
+cat("\n",digest(ser,serialize = FALSE, skip=40L))
+ser = serialize("Hello",NULL)
+cat("\n",digest(ser,serialize = FALSE, skip=40L))
+
+
+cat("\nNow ascii with and without refhook")
 ser = serialize("Hello",NULL,ascii=TRUE,refhook = function(...) "")
 cat("\n",digest(ser,serialize = FALSE, skip=digest:::set_skip(ser, TRUE)))
 ser = serialize("Hello",NULL,ascii=TRUE)
