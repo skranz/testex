f584abb1e27a771222b67b2fe8024b99417a1866
skranz <sebastian.kranz@uni-ulm.de>
2019-12-03 22:45:50 +0100

    test

--- testex/run_et.r
+++ testex/run_et.r
@@ -5,9 +5,14 @@ library(testex)
 cat("\n",digest("Hello", serializeVersion = 2))
 ser = serialize("Hello", NULL, version=2)
 cat("\n", digest(ser,serialize=FALSE, skip=14))
-
 cat("\n",digest(serialize("Hello",NULL, version=2),serialize = FALSE, skip=14))
-cat("\n",digest(serialize("Hello",NULL,refhook = function(...) NULL),serialize = FALSE, skip=14))
+cat("\nNow with refhook")
+cat("\n",digest(serialize("Hello",NULL,refhook = function(...) ""),serialize = FALSE, skip=14))
+cat("\Now ascii with and without refhook")
+ser = serialize("Hello",NULL,ascii=TRUE,refhook = function(...) "")
+cat("\n",digest(ser,serialize = FALSE, skip=digest:::set_skip(ser, TRUE)))
+ser = serialize("Hello",NULL,ascii=TRUE)
+cat("\n",digest(ser,serialize = FALSE, skip=digest:::set_skip(ser, TRUE)))
 
 
 et = readRDS("testex/et.Rds")
