b85fcb3052b4d73b6c59fdc8eaace859e635502c
skranz <sebastian.kranz@uni-ulm.de>
2019-12-03 22:55:21 +0100

    test

--- testex/run_et.r
+++ testex/run_et.r
@@ -10,11 +10,32 @@ cat("\nNow with refhook")
 cat("\n",digest(serialize("Hello",NULL,refhook = function(...) ""),serialize = FALSE, skip=14))
 
 
-cat("\nNow with and without refhook skipping 40")
+cat("\nNow with and without refhook skipping 14")
 ser = serialize("Hello",NULL,refhook = function(...) "")
-cat("\n",digest(ser,serialize = FALSE, skip=40L))
+cat("\n",digest(ser,serialize = FALSE, skip=14L))
 ser = serialize("Hello",NULL)
-cat("\n",digest(ser,serialize = FALSE, skip=40L))
+cat("\n",digest(ser,serialize = FALSE, skip=14L))
+
+
+cat("\nNow with and without refhook skipping 16")
+ser = serialize("Hello",NULL,refhook = function(...) "")
+cat("\n",digest(ser,serialize = FALSE, skip=16L))
+ser = serialize("Hello",NULL)
+cat("\n",digest(ser,serialize = FALSE, skip=16L))
+
+
+cat("\nNow with and without refhook skipping 20")
+ser = serialize("Hello",NULL,refhook = function(...) "")
+cat("\n",digest(ser,serialize = FALSE, skip=20L))
+ser = serialize("Hello",NULL)
+cat("\n",digest(ser,serialize = FALSE, skip=20L))
+
+
+cat("\nNow with and without refhook skipping 30")
+ser = serialize("Hello",NULL,refhook = function(...) "")
+cat("\n",digest(ser,serialize = FALSE, skip=30L))
+ser = serialize("Hello",NULL)
+cat("\n",digest(ser,serialize = FALSE, skip=30L))
 
 
 cat("\nNow ascii with and without refhook")
